b5e55f13d33a59d82c32ca5b71ed2259941af5a8
Riyyi <riyyi3@gmail.com>
2022-08-19 02:45:30 +0200

    Meta: Print commit hook scripts via function

--- script/lint-clang-format.sh
+++ script/lint-clang-format.sh
@@ -5,13 +5,17 @@
 
 # ------------------------------------------
 
-b="$(tput bold)"
-red="$(tput setf 4)"
-n="$(tput sgr0)"
+error() {
+	b="$(tput bold)"
+	red="$(tput setf 4)"
+	n="$(tput sgr0)"
+
+	echo "${b}${red}Error:${n} $1" >&2
+	exit 1
+}
 
 if [ ! -d ".git" ]; then
-   echo "${b}${red}Error:${n} please run this script from the project root" >&2
-   exit 1
+	error "please run this script from the project root"
 fi
 
 formatter=false
@@ -20,13 +24,11 @@ if command -v clang-format-11 >/dev/null 2>&1; then
 elif command -v clang-format >/dev/null 2>&1; then
 	formatter="clang-format"
 	if ! "$formatter" --version | awk '{ if (substr($NF, 1, index($NF, ".") - 1) < 11) exit 1; }'; then
-		echo "You are using '$("${CLANG_FORMAT}" --version)', which appears to not be clang-format 11 or later."
-		exit 1
+		error "you are using '$("${formatter}" --version)', which appears to not be clang-format 11 or later."
 	fi
 else
-	echo "clang-format-11 is not available, but C++ files need linting!"
-	echo "Either skip this script, or install clang-format-11."
-	exit 1
+	error "clang-format-11 is not available, but C++ files need linting!
+Either skip this script, or install clang-format-11."
 fi
 
 files="${1:-$(git --no-pager diff --cached --name-only)}"
--- script/lint-shell-script.sh
+++ script/lint-shell-script.sh
@@ -5,19 +5,22 @@
 
 # ------------------------------------------
 
-b="$(tput bold)"
-red="$(tput setf 4)"
-n="$(tput sgr0)"
+error() {
+	b="$(tput bold)"
+	red="$(tput setf 4)"
+	n="$(tput sgr0)"
+
+	echo "${b}${red}Error:${n} $1" >&2
+	exit 1
+}
 
 if [ ! -d ".git" ]; then
-   echo "${b}${red}Error:${n} please run this script from the project root" >&2
-   exit 1
+	error "please run this script from the project root"
 fi
 
 if ! command -v shellcheck > /dev/null 2>&1; then
-	echo "shellcheck is not available, but shell script files need linting!"
-	echo "Either skip this script, or install shellcheck."
-	exit 1
+	error "shellcheck is not available, but shell-script files need linting!
+Either skip this script, or install shellcheck."
 fi
 
 files="${1:-$(git --no-pager diff --cached --name-only)}"
