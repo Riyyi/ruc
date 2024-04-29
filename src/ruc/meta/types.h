85209522358c83dc28df2499b4d70c754121e74d
Riyyi <riyyi3@gmail.com>
2024-04-29 13:31:38 +0200

    Meta: Add type compare is() function

---
+++ src/ruc/meta/types.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023 Riyyi
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <typeinfo>
+
+template<typename T, typename U>
+inline bool is(U& input)
+{
+	if constexpr (requires { input.template fastIs<T>(); }) {
+		return input.template fastIs<T>();
+	}
+
+	return typeid(input) == typeid(T);
+}
+
+template<typename T, typename U>
+inline bool is(U* input)
+{
+	return input && is<T>(*input);
+}
